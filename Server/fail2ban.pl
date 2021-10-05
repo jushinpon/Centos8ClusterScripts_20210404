334804eba00f0b1093bbbcba854135db4aa539a6
jushinpon <jushinpon@gmail.com>
2021-10-05 11:51:52 +0800

    modify

--- Server/fail2ban.pl
+++ Server/fail2ban.pl
@@ -1,16 +1,18 @@
-system ("dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm");
+#https://rdfarm.net/fail2ban-firewalld-centos-7-cc-attacks/
+#https://snippetinfo.net/mobile/media/2570
 system ("dnf install fail2ban");
 system ("rm /etc/fail2ban/jail.local");
-system ("touch jail.local");
+#system ("mkdir -p /etc/fail2ban");
+system ("touch /etc/fail2ban/jail.local");
 $jaillocal = '/etc/fail2ban/jail.local';
-$ignoreIP = '140.117.0.0/24';
-$bantime = '999999';
+$ignoreIP = '140.117.0.0/16';
+$bantime = '86400';
 `echo "[DEFAULT]\n" >> $jaillocal`;
 `echo "ignoreip = $ignoreIP" >>$jaillocal`;
 `echo "bantime = $bantime"   >> $jaillocal`;
-`echo "findtime = 100"   >> $jaillocal`;
-`echo "maxretry = 3"   >> $jaillocal`;
-`echo "banaction = iptables-multiport"   >> $jaillocal`;
+`echo "findtime = 600"   >> $jaillocal`;
+`echo "maxretry = 10"   >> $jaillocal`;
+`echo "banaction = firewallcmd-ipset"   >> $jaillocal`;
 `echo "backend = systemd" >>  $jaillocal`;
 `echo "[sshd]">>  $jaillocal`;
 `echo "enabled = true">>  $jaillocal`;
