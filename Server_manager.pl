df2470643bd02d32925de8200efa4fc168b5a685
jushinpon <jushinpon@gmail.com>
2023-03-07 15:43:16 +0800

    modify

--- Server_manager.pl
+++ Server_manager.pl
@@ -16,7 +16,7 @@ my $adduser = "yes";
 my $setsmb = "yes";# you need to install 
 #modify /etc/fatab for /home first
 #,usrquota,grpquota then mount -a -o remount
-my $setquota = "yes"; my $quota = "100";#use df -h to check first
+my $setquota = "yes"; my $quota = "20";#use df -h to check first
 my $bsoft = int(1024*$quota)."M"; my $bhard = int(1024*$quota + 1024*5)."M";
 
 open my $ss,"< ./username.dat" or die "No Server_setting.dat to open.\n $!";#one line for an username
