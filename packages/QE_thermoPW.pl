8cd9cccf0ecdde0eb8212bc557b28b46dd26a663
jushinpon <jushinpon@gmail.com>
2023-03-22 16:09:46 +0800

    modify QE_thermoPW.pl

--- packages/QE_thermoPW.pl
+++ packages/QE_thermoPW.pl
@@ -35,7 +35,7 @@ use warnings;
 use strict;
 use Cwd; #Find Current Path
 
-my $wgetORgit = "no";## if you want to download the source, use yes. set no, if you have downloaded the source.
+my $wgetORgit = "yes";## if you want to download the source, use yes. set no, if you have downloaded the source.
 
 my $packageDir = "/home/packages";
 if(!-e $packageDir){# if no /home/packages, make this folder	
@@ -50,16 +50,16 @@ system("rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTE
 if($?){die "import intel repo key failed!\n";}
 system("yum install -y intel-mkl");
 
-my $prefix = "/opt/QEGCC_MPICH4.0.3-cp";
+my $prefix = "/opt/QEGCC_MPICH4.0.3_thermoPW";
 my $package = "q-e";
-my $currentVer = "qe-7.0.tar.gz";#***** the latest version of this package (check the latest one if possible)
-my $unzipFolder = "q-e-qe-7.0";#***** the unzipped folder of this package (check the latest one if possible)
-my $URL = "https://github.com/QEF/q-e/archive/refs/tags/qe-7.0.tar.gz";#url to download
+my $currentVer = "qe-7.1.tar.gz";#***** the latest version of this package (check the latest one if possible)
+my $unzipFolder = "q-e-qe-7.1";#***** the unzipped folder of this package (check the latest one if possible)
+my $URL = "https://github.com/QEF/q-e/archive/refs/tags/qe-7.1.tar.gz";#url to download
 my $Dir4download = "$packageDir/qe_download"; #the directory we download Mpich
 
 ## thermo_pw
 my $package1 = "ThermoPW";
-my $currentVer1 = "thermo_pw.1.7.0.tar.gz";#***** the latest version of this package (check the latest one if possible)
+my $currentVer1 = "thermo_pw.1.7.1.tar.gz";#***** the latest version of this package (check the latest one if possible)
 my $unzipFolder1 = "thermo_pw";#***** the unzipped folder of this package (check the latest one if possible)
 my $URL1 = "http://people.sissa.it/~dalcorso/thermo_pw/"."$currentVer1";#url to download
 #http://people.sissa.it/~dalcorso/thermo_pw/thermo_pw.1.7.0.tar.gz
@@ -135,13 +135,15 @@ if($?){die "**QE configure fails!\nReason:$?\n";}
 system("make clean"); 
 if($?){die "**make QE clean fails";}
 
-chdir("$Dir4download/$unzipFolder/$unzipFolder1");# cd to this dir for downloading the packages
+chdir("$Dir4download/$unzipFolder/thermo_pw");# cd to this dir for downloading the packages
 system("make join_qe");
 if($?){die "make join_qe in thermo_pw directory failed!\nReason:$?\n";}
 
 chdir("$Dir4download/$unzipFolder");# cd to this dir for downloading the packages
 
-system("make all -j $thread4make"); 
+system("make thermo_pw -j $thread4make"); 
+#system("make pw -j $thread4make"); 
+#system("make all -j $thread4make"); #cp.x
 #if($?){die "make qe failed!\nReason:$?\n";}
 sleep(1);
 system("make install"); 
